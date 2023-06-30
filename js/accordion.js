5d0e11a7fc1acf1176841efe05da44c4b861e03b
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-06-30 19:08:08 +0200

    Add files via upload

--- js/accordion.js
+++ js/accordion.js
@@ -1,24 +1,14 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const accordions = document.querySelectorAll('.accordion');
+var acc = document.getElementsByClassName("accordion");
+var i;
 
-	accordions.forEach(el => {
-		el.addEventListener('click', (e) => {
-			const self = e.currentTarget;
-			const control = self.querySelector('.accordion__control');
-			const content = self.querySelector('.accordion__content');
-
-			self.classList.toggle('open');
-
-			// если открыт аккордеон
-			if (self.classList.contains('open')) {
-				control.setAttribute('aria-expanded', true);
-				content.setAttribute('aria-hidden', false);
-				content.style.maxHeight = content.scrollHeight + 'px';
-			} else {
-				control.setAttribute('aria-expanded', false);
-				content.setAttribute('aria-hidden', true);
-				content.style.maxHeight = null;
-			}
-		});
-	});
-});
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
