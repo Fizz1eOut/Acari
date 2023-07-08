7851da45e407dcdcf94ca96bcea83bea9a6e8e39
Fizz1eOut <123012547+Fizz1eOut@users.noreply.github.com>
2023-07-08 20:35:15 +0200

    Add files via upload

--- js/accordion-anim.js
+++ js/accordion-anim.js
@@ -1,24 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-	const accordions = document.querySelectorAll('.accordion');
-
-	accordions.forEach(el => {
-		el.addEventListener('click', (e) => {
-			const self = e.currentTarget;
-			const control = self.querySelector('.accordion__control');
-			const content = self.querySelector('.accordion__content');
-
-			self.classList.toggle('open');
-
-			// если открыт аккордеон
-			if (self.classList.contains('open')) {
-				control.setAttribute('aria-expanded', true);
-				content.setAttribute('aria-hidden', false);
+	document.addEventListener('click', (e) => {
+		if (e.target.closest('.accordion__control')) {
+			console.log(e);
+			const accordion = e.target.closest('.accordion');
+			console.log(accordion);
+			const content = accordion.querySelector('.accordion__content');
+			console.log(content);
+			accordion.classList.toggle('open');
+			
+			if (accordion.classList.contains('open')) {
+				accordion.setAttribute('aria-expanded', true);
+ 				content.setAttribute('aria-hidden', false);
 				content.style.maxHeight = content.scrollHeight + 'px';
 			} else {
-				control.setAttribute('aria-expanded', false);
+				accordion.setAttribute('aria-expanded', false);
 				content.setAttribute('aria-hidden', true);
 				content.style.maxHeight = null;
 			}
-		});
-	});
-});
+		}
+	})
+})
+
